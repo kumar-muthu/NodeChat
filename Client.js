8f2788f904d6f367178c852bd418d4fcd5ba7fdb
Muthukumar Rajendran <kumar_muthu@hotmail.com>
2014-08-25 21:26:55 +0530

    Chat Files

---
+++ Client.js
@@ -0,0 +1,18 @@
+﻿var socket = io.connect('http://192.168.1.102:8081');
+socket.on('connected', function(data) {
+   
+});
+
+socket.on('chatBroadCast', function(data) {
+    $('#txtBoard').val($('#txtBoard').val() + data.name + ' : ' + data.msg + '\n');
+});
+
+var user = prompt('Hi, Please enter your name..!', '');
+$(document).ready(function () {
+    $('#Welcome').html('<b> Welcome ' + user + '...!</b>');
+    $('#btnSend').click(function() {
+        socket.emit('chatSubmit', { name: user, msg: $('#txtMsg').val() });
+        $('#txtBoard').val($('#txtBoard').val() + user + ' : ' + $('#txtMsg').val() + '\n');
+        $('#txtMsg').val('');
+    });
+});
---
+++ Server.js
@@ -0,0 +1,7 @@
+﻿var io = require('socket.io').listen(8081);
+io.sockets.on('connection', function(socket) {
+    socket.emit('connected', 'Welcome to Node Chat..!');
+    socket.on('chatSubmit', function(data) {
+        socket.broadcast.emit('chatBroadCast', data);
+    });
+});
